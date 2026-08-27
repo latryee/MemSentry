81ae074ea2c1d40a9b6c472a372fee85ca5e71a5
latryee <sariyarberkay@gmail.com>
2026-08-27 16:37:48 +0300

    fix(core): add ISO C++17/20 aligned new and delete overloads for Clang/libc++

--- src/allocator_hooks.cpp
+++ src/allocator_hooks.cpp
@@ -1,5 +1,6 @@
 #include "memsentry/core/allocator_hooks.hpp"
 #include <new>
+#include <cstdlib>
 
 void* operator new(std::size_t size) {
     void* ptr = memsentry::core::track_alloc(size);
@@ -13,20 +14,40 @@ void* operator new[](std::size_t size) {
     return ptr;
 }
 
-void operator delete(void* ptr) noexcept {
-    memsentry::core::track_free(ptr);
-}
-
-void operator delete[](void* ptr) noexcept {
-    memsentry::core::track_free(ptr);
-}
-
 void* operator new(std::size_t size, const std::nothrow_t&) noexcept {
     return memsentry::core::track_alloc(size);
 }
 
 void* operator new[](std::size_t size, const std::nothrow_t&) noexcept {
     return memsentry::core::track_alloc(size);
+}
+
+void* operator new(std::size_t size, std::align_val_t al) {
+    void* ptr = memsentry::core::track_alloc(size, static_cast<std::size_t>(al));
+    if (!ptr) throw std::bad_alloc();
+    return ptr;
+}
+
+void* operator new[](std::size_t size, std::align_val_t al) {
+    void* ptr = memsentry::core::track_alloc(size, static_cast<std::size_t>(al));
+    if (!ptr) throw std::bad_alloc();
+    return ptr;
+}
+
+void* operator new(std::size_t size, std::align_val_t al, const std::nothrow_t&) noexcept {
+    return memsentry::core::track_alloc(size, static_cast<std::size_t>(al));
+}
+
+void* operator new[](std::size_t size, std::align_val_t al, const std::nothrow_t&) noexcept {
+    return memsentry::core::track_alloc(size, static_cast<std::size_t>(al));
+}
+
+void operator delete(void* ptr) noexcept {
+    memsentry::core::track_free(ptr);
+}
+
+void operator delete[](void* ptr) noexcept {
+    memsentry::core::track_free(ptr);
 }
 
 void operator delete(void* ptr, const std::nothrow_t&) noexcept {
@@ -45,20 +66,6 @@ void operator delete[](void* ptr, std::size_t) noexcept {
     memsentry::core::track_free(ptr);
 }
 
-#if __cplusplus >= 201703L || (defined(_MSVC_LANG) && _MSVC_LANG >= 201703L)
-
-void* operator new(std::size_t size, std::align_val_t al) {
-    void* ptr = memsentry::core::track_alloc(size, static_cast<std::size_t>(al));
-    if (!ptr) throw std::bad_alloc();
-    return ptr;
-}
-
-void* operator new[](std::size_t size, std::align_val_t al) {
-    void* ptr = memsentry::core::track_alloc(size, static_cast<std::size_t>(al));
-    if (!ptr) throw std::bad_alloc();
-    return ptr;
-}
-
 void operator delete(void* ptr, std::align_val_t) noexcept {
     memsentry::core::track_free(ptr);
 }
@@ -74,5 +81,3 @@ void operator delete(void* ptr, std::size_t, std::align_val_t) noexcept {
 void operator delete[](void* ptr, std::size_t, std::align_val_t) noexcept {
     memsentry::core::track_free(ptr);
 }
-
-#endif
